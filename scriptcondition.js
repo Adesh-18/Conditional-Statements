b1ad58ed8a1b385805a77268fa8121c3dbd8719d
Vajrapu Adesh <adeshadesh645@gmail.com>
2026-05-24 09:48:51 +0530

    What's new: Conditional Statements in js.

---
+++ scriptcondition.js
@@ -0,0 +1,11 @@
+let icecream = "chocolate";
+icecream = prompt("What is your favorite flavor of ice cream?");
+if (icecream === "vanilla") {
+  console.log("Vanilla is the best flavor!");
+} else if (icecream === "chocolate") {
+  console.log("Chocolate is the best flavor!");
+} else if (icecream === "strawberry") {
+  console.log("Strawberry is the best flavor!");
+} else {
+  console.log("That is a good flavor too!");
+}
